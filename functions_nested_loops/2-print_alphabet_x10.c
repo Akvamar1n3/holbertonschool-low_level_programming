3f81ca676fad524c3ea3b1be6effedb2ee21d768
Akvamar1n3 <shiraliyevasevinj@gmail.com>
2024-10-24 11:33:21 +0000

    s

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - Prints the alphabet 10 times
+ * print_alphabet_x10 - prints the alphabet 10 times
  */
-void print_alphabet_10x(void);
+void print_alphabet_x10(void);
 {
 	int a;
 	char alph;
@@ -14,5 +14,3 @@ void print_alphabet_10x(void);
 		_putchar('\n')
 	}
 }
-
-
