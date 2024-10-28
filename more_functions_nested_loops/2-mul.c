cc0b5ae72a5d71f6fd1e7883e895120fdad4714e
Akvamar1n3 <shiraliyevasevinj@gmail.com>
2024-10-28 11:11:36 +0000

    s

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - func
+ * @a: First variable
+ * @b: second vaiable
+ *
+ * Return: result of a * b
+ */
+int mul(int a, int b)
+{
+	int res = a * b;
+
+	return (res);
+}
