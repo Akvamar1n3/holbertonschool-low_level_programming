cb66497c6404f2443d5c41ab979da9a07c91e9ae
Akvamar1n3 <shiraliyevasevinj@gmail.com>
2024-10-28 12:15:02 +0000

    s

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -5,14 +5,12 @@
  */
 void print_line(int n)
 {
-	if (n <= 0)
+	if (n > 0)
 	{
-		_putchar('\n')
+		for (int a = n; a > n; a--)
+			_putchar('_');
+		_putchar('\n');
 	}
 	else
-	{
-		for (int i = 0; i < n; i++)
-			_putchar('_');
-	}
-	_putchar('\n')
+		_putchar('\n');
 }
