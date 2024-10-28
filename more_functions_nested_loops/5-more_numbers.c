3f4a7f84353a4d79af8627b1a2ad887513aebc37
Akvamar1n3 <shiraliyevasevinj@gmail.com>
2024-10-28 11:50:19 +0000

    s

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - func
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int a = '0';
+
+	for i in range(10)
+	{
+		_putchar(a);
+		a++;
+		i++;
+	}
+	_putchar ('\n');
+}
+
