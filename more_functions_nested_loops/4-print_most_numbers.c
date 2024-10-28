93333febd17fe6ea1a838fb5c200fdb2093581cc
Akvamar1n3 <shiraliyevasevinj@gmail.com>
2024-10-28 11:37:14 +0000

    s

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - func
+ */
+void print_most_numbers(void)
+{
+	int a = '0';
+
+	while (a <= '9')
+	{
+		if (a != '2' && a != 4)
+			_putchar(a);
+		a++;
+	}
+	putchar ('\a');
+}
