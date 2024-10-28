ceefaae3615d2a93ccb55c8baa7b198704d7573f
Akvamar1n3 <shiraliyevasevinj@gmail.com>
2024-10-28 12:41:50 +0000

    s

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_square - func
+ * @size: variable
+ */
+void print_square(int size)
+{
+	int l, w;
+
+	if (size > 0)
+	{
+		for (l = 0; l < size; l++)
+		{
+			for (w = 0; w < size; w++)
+				_putchar('#');
+			_putchar('\n');
+		}
+
+	}
+	else
+		_putchar('\n');
+}
+
+
