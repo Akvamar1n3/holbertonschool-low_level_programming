5756a23d1738deba15671d46fbecd90cfe9ab6f0
Akvamar1n3 <shiraliyevasevinj@gmail.com>
2024-10-28 12:57:57 +0000

    s

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * main - func
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	while (a++ < 100)
+	{
+		if ((a % 3) == 0 && (a % 5) == 0)
+			printf("FizzBuzz ");
+		else if ((a % 5) == 0)
+		{
+			if (a == 100)
+			{
+				printf("Buzz");
+				printf("\n");
+			}
+			else
+				printf("Buzz ");
+		}
+		else if ((a % 3) == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", a);
+	}
+	return (0);
+}
+
+
+
