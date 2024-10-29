8506016742906e3a5fbc35da05850f31cdfeeff4
Akvamar1n3 <shiraliyevasevinj@gmail.com>
2024-10-29 12:00:48 +0000

    s

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - func
+ * @str: pointer
+ */
+void _puts(char *str)
+{
+	int a = 0;
+	
+	while(str[a] !='\0')
+	{
+		_putchar(str[a]);
+		a++;
+
+	}
+	_putchar('\n');
+}
+
