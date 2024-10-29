943d029415c5ed250a74cc8a36bfb1169b656d1f
Akvamar1n3 <shiraliyevasevinj@gmail.com>
2024-10-29 11:33:26 +0000

    s

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_int - func
+ * @a: integer
+ * @b: integer
+ */
+
+void swap_int(int *a, int *b);
+{
+	int temp;
+
+	temp = *b;
+	*b = *a;
+	*a = temp;
+}
