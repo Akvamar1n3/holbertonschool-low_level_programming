690d8264ce271f8789ae69796eac802b19cabd25
Akvamar1n3 <shiraliyevasevinj@gmail.com>
2024-10-29 11:50:27 +0000

    s

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -2,15 +2,15 @@
 /**
  * _strlen - func
  * @s: pointer
- *
  * Return: length of a string
  */
-void _strlen(char *s)
+int _strlen(char *s)
 {
 	int a = 0;
 
-	while(s[a] != '\0')
+	while (s[a] != '\0')
 		a++;
 
 	return (a);
+
 }
