e6ca441435e6f7cdd823d76c3ba168d155d7b979
Akvamar1n3 <shiraliyevasevinj@gmail.com>
2024-10-31 11:00:49 +0000

    s

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * strcat - func
+ * @dest: pointer
+ * @src: pointer
+ * Return: sum 2 of this
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a = 0, b = 0;
+
+	while (dest[a] != '\0')
+		a++;
+	while (src[b] != '\0')
+		dest[a++] = src[b++];
+	dest[a] = '\0';
+
+	return (dest);
+}
